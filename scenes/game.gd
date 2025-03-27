c64d1fef807d64f2e134fbc7d379af5ec9f3c52c
trui0002 <trui0002@gmail.com>
2025-03-27 17:09:52 +0200

    made the enemy spawn to work acording to the gdd

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,14 +1,33 @@
 extends Node2D
 @export var enemy_scene: PackedScene
 @onready var spawn_timer = $SpawnTimer 
+@export var spawnTime: float = 4.0  # Initial spawn interval (seconds)
+@export var minSpawnTime: float = 0.5  # Minimum interval (limit)
+@export var spawnDecreaseRate: float = 0.1  # How much to decrease each time
+
+
+
 
 func _ready():
 	spawn_timer.timeout.connect(spawn_enemy)  # Call spawn_enemy() when the timer runs out
-	spawn_timer.start(3)  # Spawns an enemy every 3 seconds
+	spawn_timer.start(spawnTime) 
+	
+
+
 
 func spawn_enemy():
-	var spawn_points = get_tree().get_nodes_in_group("spawn_points")
-	var spawn_point = spawn_points.pick_random()  # Choose a random marker
+	var min_distance = 800  # Minimum distance from base
+	var max_distance = 1700  # Maximum distance from base
+	var playerHub = get_node("Hub")
+	var random_position = get_random_position(playerHub.global_position, min_distance, max_distance)
 	var enemy_instance = enemy_scene.instantiate()
-	enemy_instance.position = spawn_point.position  # Set enemy position
+	enemy_instance.global_position = random_position
 	add_child(enemy_instance) 
+	
+func get_random_position(center: Vector2, min_distance: float, max_distance: float)-> Vector2:
+	spawnTime = max(spawnTime - spawnDecreaseRate, minSpawnTime)
+	spawn_timer.start(spawnTime)
+	var angle = randf_range(0, TAU)
+	var distance = randf_range(min_distance, max_distance)
+	var offset =Vector2(cos(angle), sin(angle)) * distance
+	return center + offset
